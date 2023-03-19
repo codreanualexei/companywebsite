ec99671edb074acd31633bedd67ca572f1949d6e
MeryAmun <chebesiawah@gmail.com>
2023-03-19 02:17:29 +0100

    another one

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export {default as Navbar } from './Navbar/Navbar'
-export {default as ImageContainer } from './imageComponent/ImageComponent';
 export { default as Footer } from './footer/Footer'
 export { default as OpenSource } from './openSource/OpenSource'
 export { default as News } from './news/News';
