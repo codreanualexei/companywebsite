fb8988866298a9cd060181a9eab48b94bd58c4e0
Your Name <yo.com>
2024-04-20 10:54:31 +0300

    first commit

--- src/staticData/staticData.js
+++ src/staticData/staticData.js
@@ -1,69 +1,146 @@
 // import DiamondOutlinedIcon from '@mui/icons-material/DiamondOutlined';
 // import AppleIcon from '@mui/icons-material/Apple';
 // import GroupsIcon from '@mui/icons-material/Groups';
-import { AiFillApple, AiOutlineAndroid } from 'react-icons/ai'
-import {SlGlobe } from 'react-icons/sl'
-import { HiOutlineUserGroup} from 'react-icons/hi';
-import { andre, claudio, daniel, logo1,logo2,logo3,logo4,logo5,logo6,logo7,logo9,logo11,logo12,logo13,logo14,logo15,logo10,logo8,logo16, one, three, four, five, six, seven, eight, nine, twelve, thirteen, ten, eleven
- } from '../assets';
- import { FaAssistiveListeningSystems } from "react-icons/fa";
- import { MdOutlineSecurityUpdateGood } from "react-icons/md";
- import { GiDiamondHard } from "react-icons/gi";
+import { AiFillApple, AiOutlineAndroid } from "react-icons/ai";
+import { SlGlobe } from "react-icons/sl";
+import { HiOutlineUserGroup } from "react-icons/hi";
+import {
+  andre,
+  claudio,
+  daniel,
+  logo1,
+  logo2,
+  logo3,
+  logo4,
+  logo5,
+  logo6,
+  logo7,
+  logo9,
+  logo11,
+  logo12,
+  logo13,
+  logo14,
+  logo15,
+  logo10,
+  logo8,
+  logo16,
+  one,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine,
+  twelve,
+  thirteen,
+  ten,
+  eleven,
+} from "../assets";
+import { FaAssistiveListeningSystems } from "react-icons/fa";
+import { MdOutlineSecurityUpdateGood } from "react-icons/md";
+import { GiDiamondHard } from "react-icons/gi";
 
 export const serviceOptions = [
-    {
-        icon:<SlGlobe fontSize='50px' color='#25940ee1'/>,link:'/mobile-web-apps', title:'Full Stack Web Applications',text:`A structured 7-day Process to align your vision with your business goals`
-    },
-    {
-        icon:<AiFillApple fontSize='50px' color='#25940ee1'/>,link:'/mobile-ios-apps', title:'Full Stack IOS Mobile Applications',text:'Design an engaging product that is easy-to-use, attractive and functional'
-    },
-   
-    {
-        icon:<AiOutlineAndroid fontSize='50px' color='#25940ee1'/>,link:'/mobile-android-apps', title:'Full Stack Android Mobile Applications',text:`Go to market quickly with a high-quality Minimum Viable Product`
-    },
-    {
-        icon:<HiOutlineUserGroup fontSize='50px' color='#25940ee1'/>,link:'teams', title:'Dedicated Teams',text:`Build your product with a dedicated team of developers`
-    },
-]
-export const aboutUsGoals = [
-    {
-        icon:<GiDiamondHard fontSize='50px' color='#25940ee1'/>,title:'Hard Work', text:'We are a team of hardworking, innovative IT consultants who combine our strengths together so that we can offer you the best services.'
-    },
-    {
-        icon:<FaAssistiveListeningSystems fontSize='50px' color='#25940ee1'/>,title:'Attention to Details', text:'We genuinely listen to your needs and objectives,and create unique strategies to achieve your vision.'
-    },
-   
-    {
-        icon:<MdOutlineSecurityUpdateGood fontSize='50px' color='#25940ee1'/>,title:'Excellence', text:'We pride ourselves in our excellence and inventiveness.'
-    },
-]
+  {
+    icon: <SlGlobe fontSize="50px" color="#25940ee1" />,
+    link: "/",
+    title: "Full Stack Web Applications",
+    text: `A structured 7-day Process to align your vision with your business goals`,
+  },
+  {
+    icon: <AiFillApple fontSize="50px" color="#25940ee1" />,
+    link: "/",
+    title: "Full Stack IOS Mobile Applications",
+    text: "Design an engaging product that is easy-to-use, attractive and functional",
+  },
 
+  {
+    icon: <AiOutlineAndroid fontSize="50px" color="#25940ee1" />,
+    link: "/",
+    title: "Full Stack Android Mobile Applications",
+    text: `Go to market quickly with a high-quality Minimum Viable Product`,
+  },
+  {
+    icon: <HiOutlineUserGroup fontSize="50px" color="#25940ee1" />,
+    link: "/",
+    title: "Dedicated Teams",
+    text: `Build your product with a dedicated team of developers`,
+  },
+];
+export const aboutUsGoals = [
+  {
+    icon: <GiDiamondHard fontSize="50px" color="#25940ee1" />,
+    title: "Hard Work",
+    text: "We are a team of hardworking, innovative IT consultants who combine our strengths together so that we can offer you the best services.",
+  },
+  {
+    icon: <FaAssistiveListeningSystems fontSize="50px" color="#25940ee1" />,
+    title: "Attention to Details",
+    text: "We genuinely listen to your needs and objectives,and create unique strategies to achieve your vision.",
+  },
+
+  {
+    icon: <MdOutlineSecurityUpdateGood fontSize="50px" color="#25940ee1" />,
+    title: "Excellence",
+    text: "We pride ourselves in our excellence and inventiveness.",
+  },
+];
 
 export const team = [
-    {
-        image:andre, title:'Need a Team?', text:'Need a dynamic team? Then you should talk to me. I’d be glad to discuss your needs with you & help you build a team of top talent to help your business reach its potential.',
-        link:'Email Andre'
-    },
-    {
-        image:daniel, title:'Building an MVP?', text:'Looking to build fast and reduce your time to market? Drop me a message. Together we can find the core set of features to gather user information and iteratively create the best possible product.',
-        link:'Email Daniel'
-    },
-    {
-        image:claudio, title:'Talk Tech?', text:'If you want to have a jargon-free tech conversation with someone who speaks your language then drop me a message. I can help you find the right technical requirements for your product.',
-        link:'Email Claudio'
-    },
-]
+  {
+    image: andre,
+    title: "Need a Team?",
+    text: "Need a dynamic team? Then you should talk to me. I’d be glad to discuss your needs with you & help you build a team of top talent to help your business reach its potential.",
+    link: "Email Andre",
+  },
+  {
+    image: daniel,
+    title: "Building an MVP?",
+    text: "Looking to build fast and reduce your time to market? Drop me a message. Together we can find the core set of features to gather user information and iteratively create the best possible product.",
+    link: "Email Daniel",
+  },
+  {
+    image: claudio,
+    title: "Talk Tech?",
+    text: "If you want to have a jargon-free tech conversation with someone who speaks your language then drop me a message. I can help you find the right technical requirements for your product.",
+    link: "Email Claudio",
+  },
+];
 export const awards = [
-    thirteen,one,three,five,twelve,four,eight,nine,ten,eleven, six,seven
-]
+  thirteen,
+  one,
+  three,
+  five,
+  twelve,
+  four,
+  eight,
+  nine,
+  ten,
+  eleven,
+  six,
+  seven,
+];
 export const clients = [
-    logo1,logo2,logo3,logo4,logo5,logo6,logo7,logo9,logo11,logo12,logo13,logo14,logo15,logo10,logo8,logo16
-]
+  logo1,
+  logo2,
+  logo3,
+  logo4,
+  logo5,
+  logo6,
+  logo7,
+  logo9,
+  logo11,
+  logo12,
+  logo13,
+  logo14,
+  logo15,
+  logo10,
+  logo8,
+  logo16,
+];
 
-
-
-
-// const footerLinks ={ 
+// const footerLinks ={
 //     global:[
 //         {name:'Work',link:'/work'},
 //         {name:'About Us',link:'/about'},
@@ -93,30 +170,28 @@ export const clients = [
 // }
 
 export const newsData = [
-    {
-       title:'Altar Named Leading Global Company in Manifest 2022 Awards',text:'We are thrilled to share that we have been recognized as a global industry leader by The Manifest in their 2022 awards. We are grateful to be recognized among such a talented group of companies. We look forward to continuing to provide top-quality products and services to our customers in the coming year.'
-
-    },
-    {
-       title:'Altar Named Top B2B Company by Manifest',text:'We are thrilled to share that we have been recognized as a global industry leader by The Manifest in their 2022 awards. We are grateful to be recognized among such a talented group of companies. We look forward to continuing to provide top-quality products and services to our customers in the coming year.'
-
-    },
-    {
-       title:'Altar CO-Founder & CEO interviewed by Forbes',text:'We are thrilled to share that we have been recognized as a global industry leader by The Manifest in their 2022 awards. We are grateful to be recognized among such a talented group of companies. We look forward to continuing to provide top-quality products and services to our customers in the coming year.'
-
-    },
-    {
-       title:'Fast Company Recognizes Altar Client in 2022  Awards',text:'We are thrilled to share that we have been recognized as a global industry leader by The Manifest in their 2022 awards. We are grateful to be recognized among such a talented group of companies. We look forward to continuing to provide top-quality products and services to our customers in the coming year.'
-
-    },
-    {
-       title:'Altar NamedTop 1000 Company by Clutch',text:'We are thrilled to share that we have been recognized as a global industry leader by The Manifest in their 2022 awards. We are grateful to be recognized among such a talented group of companies. We look forward to continuing to provide top-quality products and services to our customers in the coming year.'
-
-    },
-    {
-       title:'Altar CO-Founder & CEO interviewed by Business Magazine',text:'We are thrilled to share that we have been recognized as a global industry leader by The Manifest in their 2022 awards. We are grateful to be recognized among such a talented group of companies. We look forward to continuing to provide top-quality products and services to our customers in the coming year.'
-
-    },
-    
-   
-]
+  {
+    title: "Altar Named Leading Global Company in Manifest 2022 Awards",
+    text: "We are thrilled to share that we have been recognized as a global industry leader by The Manifest in their 2022 awards. We are grateful to be recognized among such a talented group of companies. We look forward to continuing to provide top-quality products and services to our customers in the coming year.",
+  },
+  {
+    title: "Altar Named Top B2B Company by Manifest",
+    text: "We are thrilled to share that we have been recognized as a global industry leader by The Manifest in their 2022 awards. We are grateful to be recognized among such a talented group of companies. We look forward to continuing to provide top-quality products and services to our customers in the coming year.",
+  },
+  {
+    title: "Altar CO-Founder & CEO interviewed by Forbes",
+    text: "We are thrilled to share that we have been recognized as a global industry leader by The Manifest in their 2022 awards. We are grateful to be recognized among such a talented group of companies. We look forward to continuing to provide top-quality products and services to our customers in the coming year.",
+  },
+  {
+    title: "Fast Company Recognizes Altar Client in 2022  Awards",
+    text: "We are thrilled to share that we have been recognized as a global industry leader by The Manifest in their 2022 awards. We are grateful to be recognized among such a talented group of companies. We look forward to continuing to provide top-quality products and services to our customers in the coming year.",
+  },
+  {
+    title: "Altar NamedTop 1000 Company by Clutch",
+    text: "We are thrilled to share that we have been recognized as a global industry leader by The Manifest in their 2022 awards. We are grateful to be recognized among such a talented group of companies. We look forward to continuing to provide top-quality products and services to our customers in the coming year.",
+  },
+  {
+    title: "Altar CO-Founder & CEO interviewed by Business Magazine",
+    text: "We are thrilled to share that we have been recognized as a global industry leader by The Manifest in their 2022 awards. We are grateful to be recognized among such a talented group of companies. We look forward to continuing to provide top-quality products and services to our customers in the coming year.",
+  },
+];
